0d14822d1a4067e90d12765496714041915e4122
Francis Aiello <aielfa@farmingdale.edu>
2024-11-25 10:42:52 -0500

    Completed ToDo 1-2

--- app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import edu.farmingdale.draganddropanim_demo.ui.theme.DragAndDropAnim_DemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,25 +16,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DragAndDropAnim_DemoTheme {
-
-                    DragAndDropBoxes()
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DragAndDropBoxes(Modifier.padding(innerPadding))
 
             }
         }
     }
 }
+}
 
 
 
 // This should be completed in a group setting
-// ToDo x: Analyze the requirements for Individual Project 3
-// ToDo x: Show the DragAndDropBoxes composable
-// ToDo x: Change the circle to a rect
-// ToDo x: Replace the command right with a image or icon
-// ToDo x: Make this works in landscape mode only
-// ToDo x: Rotate the rect around itself
-// ToDo x: Move - translate the rect horizontally and vertically
+// ToDo 1: Analyze the requirements for Individual Project 3
+// ToDo 2: Show the DragAndDropBoxes composable
+// ToDo 3: Change the circle to a rect
+// ToDo 4: Replace the command right with a image or icon
+// ToDo 5: Make this works in landscape mode only
+// ToDo 6: Rotate the rect around itself
+// ToDo 7: Move - translate the rect horizontally and vertically
 // ToDo 8: Add a button to reset the rect to the center of the screen
 // ToDo 9: Enable certain animation based on the drop event (like up or down)
 // ToDo 10: Make sure to commit for each one of the above and submit this individually
